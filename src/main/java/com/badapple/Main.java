b53cb1b08e8d8e382768883a3c2e5ef3c23a62c3
xoko14 <xoquinperezb@gmail.com>
2021-12-09 12:48:12 +0100

    a bit of refactoring & new stuff that doesn't work

--- src/main/java/com/badapple/Main.java
+++ src/main/java/com/badapple/Main.java
@@ -3,6 +3,7 @@ package com.badapple;
 
 public class Main {
     public static void main(String[] args) {
-        Window.init();
+        Thread mainWindow = new Thread(new Window());
+        mainWindow.start();
     }
 }
--- src/main/java/com/badapple/Window.java
+++ src/main/java/com/badapple/Window.java
@@ -5,12 +5,14 @@ import com.badapple.utils.GZipBytes;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferStrategy;
 import java.util.concurrent.locks.LockSupport;
 
-public class Window {
+public class Window extends JFrame implements Runnable {
 
     private static final int WIDTH = 96;
     private static final int HEIGHT = 64;
@@ -18,76 +20,97 @@ public class Window {
     private static final int WINDOW_WIDTH = WIDTH * 2;
     private static final int WINDOW_HEIGHT = HEIGHT * 2;
 
-    private static JFrame frame;
-    private static Canvas canvas;
-    private static BufferStrategy bufferStrategy;
+    private final Canvas canvas;
+    private final BufferStrategy bufferStrategy;
 
-    private static GZipBytes video;
-    private static boolean closed = false;
-    private static boolean focused;
+    private final GZipBytes video;
+    private boolean closed = false;
 
-    public static void init(){
-        Window.video = new GZipBytes(Window.class.getResourceAsStream("/frames.bin.gz"));
+    public Window(){
+        video = new GZipBytes(Window.class.getResourceAsStream("/frames.bin.gz"));
+        this.setTitle("Bad Apple");
+        this.getContentPane().setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_Q) {
+                    System.exit(0);
+                } else {
+                    System.out.println("Key not mapped");
+                }
+            }
 
-        Window.frame = new JFrame("Bad Apple");
-        Window.frame.getContentPane().setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
-        Window.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        // Window.frame.setResizable(false);
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println("Key pressed "+ e.getKeyCode());
+            }
 
-        Window.canvas = new Canvas();
-        Window.canvas.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
-        Window.canvas.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        Window.canvas.setIgnoreRepaint(true);
-        Window.frame.add(canvas);
-        Window.frame.pack();
-        Window.frame.setLocationRelativeTo(null);
-        Window.frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println("Key released"+ e.getKeyCode());
+            }
+        });
+        this.setFocusable(true);
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        canvas.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        canvas.setIgnoreRepaint(true);
+        this.add(canvas);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 super.windowClosing(e);
-                Window.closed = true;
+                closed = true;
             }
 
             @Override
             public void windowGainedFocus(WindowEvent e) {
                 super.windowGainedFocus(e);
-                Window.focused = true;
             }
 
             @Override
             public void windowLostFocus(WindowEvent e) {
                 super.windowLostFocus(e);
-                Window.focused = false;
             }
         });
 
-        Window.canvas.createBufferStrategy(2);
-        Window.bufferStrategy = Window.canvas.getBufferStrategy();
-
-        Window.frame.setVisible(true);
-        Window.canvas.setVisible(true);
-
-        PlayAudio play = new PlayAudio();
-        play.start();
-
-        int totalFrames = video.getLength() / (WIDTH * HEIGHT);
-
-        for (int i = 0; i < totalFrames || Window.closed; i++) {
-            long time1 = System.nanoTime();
-            renderFrame(i, totalFrames);
-            long time2 = System.nanoTime();
-            LockSupport.parkNanos(1000000000 / 30 - (time2 - time1)); // fps (duration of frame in nanosec - time taken to render frame)
-        }
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
     }
 
-    private static void renderFrame(int frame, int totalFrames){
+    @Override
+    public void run(){
+        this.setVisible(true);
+        canvas.setVisible(true);
+
+        new Thread(() -> {
+            PlayAudio play = new PlayAudio();
+            play.start();
+
+            int totalFrames = video.getLength() / (WIDTH * HEIGHT);
+
+            for (int i = 0; i < totalFrames || closed; i++) {
+                long time1 = System.nanoTime();
+                renderFrame(i, totalFrames);
+                long time2 = System.nanoTime();
+                LockSupport.parkNanos(1000000000 / 30 - (time2 - time1)); // fps (duration of frame in nanosec - time taken to render frame)
+            }
+        }).start();
+        System.out.println("I reached here");
+    }
+
+    private void renderFrame(int frame, int totalFrames){
         Graphics graphics = bufferStrategy.getDrawGraphics();
 
         double rendererAspect = (double) WIDTH / (double) HEIGHT,
-                windowAspect = (double)Window.canvas.getWidth() / (double) Window.canvas.getHeight();
+                windowAspect = (double)canvas.getWidth() / (double) canvas.getHeight();
         int scaleFactor = windowAspect > rendererAspect ?
-                Window.canvas.getHeight() / HEIGHT :
-                Window.canvas.getWidth() / WIDTH;
+                canvas.getHeight() / HEIGHT :
+                canvas.getWidth() / WIDTH;
 
         int rw = (WIDTH * scaleFactor), rh = (HEIGHT * scaleFactor);
 
@@ -95,12 +118,12 @@ public class Window {
 
         // clear buffer
         graphics.setColor(Color.GRAY);
-        graphics.fillRect(0,0, Window.frame.getWidth(), Window.frame.getHeight());
+        graphics.fillRect(0,0, canvas.getWidth(), canvas.getHeight());
 
         // draw frame
-        int y = (Window.canvas.getHeight()-rh)/2;
+        int y = (canvas.getHeight()-rh)/2;
         for (int i = 0; i < HEIGHT * WIDTH; i += WIDTH) {
-            int x = (Window.canvas.getWidth()-rw)/2;
+            int x = (canvas.getWidth()-rw)/2;
             for (int j = startbyte + i; j < startbyte + WIDTH + i; j++) {
                 if (video.getByte(j) == 0) graphics.setColor(Color.BLACK);
                 else graphics.setColor(Color.WHITE);
@@ -122,14 +145,14 @@ public class Window {
 
         // draw progressbar
         graphics.setColor(Color.RED);
-        double barSize =  (double) frame/(double) totalFrames * Window.canvas.getWidth();
-        graphics.fillRect(0,Window.canvas.getHeight()-4, (int) barSize, 4);
+        double barSize =  (double) frame/(double) totalFrames * canvas.getWidth();
+        graphics.fillRect(0,canvas.getHeight()-4, (int) barSize, 4);
 
         graphics.dispose();
 
         // show buffer on canvas
-        if (!Window.bufferStrategy.contentsLost()) {
-            Window.bufferStrategy.show();
+        if (!bufferStrategy.contentsLost()) {
+            bufferStrategy.show();
         }
     }
 }
