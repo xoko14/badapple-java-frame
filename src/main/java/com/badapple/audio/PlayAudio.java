20c7d102cd9ef548c4370765721c7cf78b2549bf
xoko14 <xoquinperezb@gmail.com>
2022-10-17 18:21:41 +0200

    load audio beforehand

--- src/main/java/com/badapple/audio/PlayAudio.java
+++ src/main/java/com/badapple/audio/PlayAudio.java
@@ -4,14 +4,22 @@ import java.io.InputStream;
 import javazoom.jl.player.advanced.AdvancedPlayer;
 
 public class PlayAudio extends Thread{
+    private AdvancedPlayer player;
+
+    public PlayAudio(){
+        InputStream file = getClass().getResourceAsStream("/badapple.mp3");
+        assert file != null;
+        try {
+            player = new AdvancedPlayer(file);
+        } catch (Exception e) {
+            System.out.println("Error loading audio!");
+        }
+    }
     public void run(){
         try {
-            InputStream file = getClass().getResourceAsStream("/badapple.mp3");
-            assert file != null;
-            AdvancedPlayer player = new AdvancedPlayer(file);
             player.play();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Error playing audio!");
         }
     }
 }
